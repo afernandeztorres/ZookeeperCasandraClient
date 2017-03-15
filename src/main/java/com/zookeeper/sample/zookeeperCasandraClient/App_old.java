bf3d080e11f5900e2ca3ea3b6bdeabec5d0c0edf
Alberto Fernández Torres <alberto.fernandez@panel.es>
2017-03-15 13:02:24 +0100

    Mavenized Project

---
+++ src/main/java/com/zookeeper/sample/zookeeperCasandraClient/App_old.java
@@ -0,0 +1,13 @@
+package com.zookeeper.sample.zookeeperCasandraClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App_old 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
